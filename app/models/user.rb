bafa9055755dc7662d08e0fb7032710d73b4047a
Shay Craig <71286979+craigs40@users.noreply.github.com>
2021-07-07 20:04:21 -0400

    Add controller, update models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :events
+  has_many :attended_events, foreign_key: :attended_event_id, class_name: "Event"
 end
