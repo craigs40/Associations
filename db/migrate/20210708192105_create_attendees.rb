2c878799fde7d83bc411124b0a385fa84d44ceef
Shay Craig <craig.shay40@gmail.com>
2021-07-15 00:37:10 -0400

    Add migration, add webpacker

--- db/migrate/20210708192105_create_attendees.rb
+++ db/migrate/20210708192105_create_attendees.rb
@@ -1,8 +1,8 @@
 class CreateAttendees < ActiveRecord::Migration[6.1]
   def change
     create_table :attendees do |t|
-
-      t.timestamps
+      t.integer :attendee_id
+      t.timestampss
     end
   end
 end
